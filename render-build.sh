14ba8fdbfae7fb809b861cc5634ff92548e16773
Youssef-Ben-Amor-Cherif <youssefcherif12345@outlook.fr>
2024-09-12 16:37:17 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
+# Chrome installation (no changes)
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
@@ -11,7 +12,7 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Make sure we return to where we were
+  cd $HOME/project/src
 else
   echo "...Using Chrome from cache"
 fi
@@ -19,13 +20,11 @@ fi
 # Upgrade pip, setuptools, and wheel
 pip3 install --upgrade pip setuptools wheel
 
-# Install system dependencies for building numpy and other packages
-apt-get update && apt-get install -y build-essential libatlas-base-dev gfortran
+# Install system dependencies for numpy and other packages
+apt-get update && apt-get install -y build-essential libatlas-base-dev
 
 # Install Python dependencies
 pip3 install -r requirements.txt
 
-# Ensure Chrome is added to the PATH
+# Ensure Chrome is added to PATH
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
-
-# Additional build steps
