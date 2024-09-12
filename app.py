aaa04de6b1a4c63b84b168b634ebcdb956bc4040
mehdIDerrick <139765929+mehdIDerrick@users.noreply.github.com>
2024-09-12 15:45:26 +0100

    Update app.py update render service

--- app.py
+++ app.py
@@ -44,8 +44,25 @@ def scroll_and_click(driver, element, retries=3):
     print(f"Failed to click the element after {retries} attempts.")
     return False
 
+#chrome_options = Options()
+#chrome_options.add_argument("--headless")
+
+#chrome_options.add_argument("--window-size=2560,1440")
+#chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+#chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+#chrome_options.add_argument("--proxy-server='direct://'")
+#chrome_options.add_argument("--proxy-bypass-list=*")
+#chrome_options.add_argument("--start-maximized")
+#chrome_options.add_argument("--disable-dev-shm-usage")
+#chrome_options.add_argument("--no-sandbox")
+#chrome_options.add_argument("--start-maximized")
+
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument('--headless')  # Ensure headless mode
+#chrome_options.add_argument('--no-sandbox')  # Bypass OS security model, required in cloud environments
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 chrome_options.add_argument("--window-size=2560,1440")
 chrome_options.add_argument("--disable-gpu")
@@ -56,9 +73,11 @@ chrome_options.add_argument("--proxy-server='direct://'")
 chrome_options.add_argument("--proxy-bypass-list=*")
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--start-maximized")
 
+
+service = Service(ChromeDriverManager().install(), port=0)  # Let OS pick an open port
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Initialize WebDriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
@@ -486,4 +505,5 @@ def download_csv():
         return jsonify({"error": "File not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get the port from environment or default to 5000
+    app.run(host='0.0.0.0', port=port)
