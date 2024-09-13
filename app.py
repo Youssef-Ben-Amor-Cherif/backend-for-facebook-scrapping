29cfbedbd20cba20be39c66d81980c444be469cf
Youssef-Ben-Amor-Cherif <youssefcherif12345@outlook.fr>
2024-09-13 10:35:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -510,4 +510,4 @@ def download_csv():
 if __name__ == '__main__':
 
     port = int(os.environ.get('PORT', 5000))  # Get the port from environment or default to 5000
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port)
