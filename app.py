58e260e9607627700a5944e3ed352416b6c067ae
Youssef-Ben-Amor-Cherif <youssefcherif12345@outlook.fr>
2024-09-13 12:11:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,6 @@ def scroll_and_click(driver, element, retries=3):
 #chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 #chrome_options.add_argument("--proxy-server='direct://'")
 #chrome_options.add_argument("--proxy-bypass-list=*")
-#chrome_options.add_argument("--start-maximized")
 #chrome_options.add_argument("--disable-dev-shm-usage")
 #chrome_options.add_argument("--no-sandbox")
 #chrome_options.add_argument("--start-maximized")
