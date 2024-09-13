a6f815a61ef011f6bca03ead5a8be6e588a9eb8e
Youssef-Ben-Amor-Cherif <youssefcherif12345@outlook.fr>
2024-09-13 12:17:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -72,17 +72,13 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 chrome_options.add_argument("--proxy-server='direct://'")
 chrome_options.add_argument("--proxy-bypass-list=*")
-chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument('--single-process')  # Run as a single process
-chrome_options.add_argument('--disable-software-rasterizer')  # Disable 3D graphics
 chrome_options.add_argument('--disable-software-rasterizer')
-chrome_options.add_argument('--remote-debugging-port=9222')
 # Chrome flags to reduce memory consumption
 chrome_options.add_argument('--disable-background-timer-throttling')
 chrome_options.add_argument('--disable-backgrounding-occluded-windows')
-chrome_options.add_argument('--disable-renderer-backgrounding')
+
 
 service = Service(ChromeDriverManager().install(), port=0)  # Let OS pick an open port
 driver = webdriver.Chrome(service=service, options=chrome_options)
