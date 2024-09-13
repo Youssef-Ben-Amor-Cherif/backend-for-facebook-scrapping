ed0b1cb4d1cee7be5266dc585bc9481586688dcd
Youssef-Ben-Amor-Cherif <youssefcherif12345@outlook.fr>
2024-09-13 12:21:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,6 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 chrome_options.add_argument("--proxy-server='direct://'")
 chrome_options.add_argument("--proxy-bypass-list=*")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument('--single-process')  # Run as a single process
 chrome_options.add_argument('--disable-software-rasterizer')
 # Chrome flags to reduce memory consumption
 chrome_options.add_argument('--disable-background-timer-throttling')
